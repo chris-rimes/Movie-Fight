79e403da7e04b610d848ece116e08e59b59329ec
Chris Rimes <56796061+chris-rimes@users.noreply.github.com>
2020-10-10 21:00:03 -0700

    updated axios request

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const autoCompleteConfig = {
     return movie.Title;
   },
   async fetchData(searchTerm) {
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('https://www.omdbapi.com/', {
       params: {
         apikey: 'ab5e6ad0',
         s: searchTerm,
@@ -47,7 +47,7 @@ let leftMovie;
 let rightMovie;
 
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
+  const response = await axios.get('https://www.omdbapi.com/', {
     params: {
       apikey: 'ab5e6ad0',
       i: movie.imdbID,
